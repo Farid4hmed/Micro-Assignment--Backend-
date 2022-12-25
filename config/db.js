d18ed4bb20d9cc26b82374c5d35d6555adea3f4c
Farid Ahmed <faridahmed.tech@gmail.com>
2022-12-25 15:24:02 +0530

    added database models

--- config/db.js
+++ config/db.js
@@ -1,8 +1,15 @@
-//mongoose connection
+const mongoose = require("mongoose");
+const mongoURL = process.env.MONGODB_URI;
+
+function databaseConnection(){
+    mongoose.set("strictQuery", true);
 mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true, })
 .then(() => {
     console.log("Database connected successfully");
 })
-.catch(() => {
+.catch((err) => {
     console.log("Database not connected successfully: " + err);
 });
+}
+
+module.exports = databaseConnection;
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
+const initDatabase = require("./config/db.js");
 
-const mongoURL = process.env.MONGODB_URL;
+initDatabase();
+
+//Schemas for Category and image
+const galleryCategory = new mongoose.Schema(
+   { name:{type:String, required:true}},
+    {timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" }}
+);
+const imagesGallery = new mongoose.Schema(
+    { name: {
+            type: String,
+            required: true,
+        },
+        category: { type: Array, required: true },
+        likes: { type: Number, default: 0 },
+        imageUrl: { type: String, required: true },
+    },
+    { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
+
+//models
+const Category = mongoose.model("Category", galleryCategory);
+const Image = mongoose.model("Image", imagesGallery);
 
 
-const port = process.env.PORT || 8000;
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
 const host = process.env.HOST || "localhost";
 
 app.use((req, res) => {
