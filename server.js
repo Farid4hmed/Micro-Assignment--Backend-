38a475d1a5d3bd84c5719f63ed7097d133b5a99a
Farid Ahmed <faridahmed.tech@gmail.com>
2022-12-28 12:43:26 +0530

    added 404 route not found middleware

--- server.js
+++ server.js
@@ -36,20 +36,14 @@ app.use("/api/admin", admin);
 
 
 
-//Error Handler Middleware
-app.use((req, res, next) => {
-    const err = new Error("Not found");
-    err.status = 404;
-    next(err);
-});
+// route not found middleware
+app.use((req, res, next) =>
+    res.status(404).send("You are looking for something that we not have!")
+);
+
+//error handler middleware
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message
-        }
-    });
+    res.status(500).send("Something went wrong!");
 });
 
 
